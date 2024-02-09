f876cfac1465c77bb589393e0213867217b1aba3
Glen Alexander <49417961+glenax@users.noreply.github.com>
2024-02-09 20:37:29 +1300

    Handle location insights without a name

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -102,9 +102,15 @@ def fetch_location_insight_data():
     json = r.json()
     results = []
     for location_insight in json["locations"]:
+        address = ""
+        name = ""
+        if "name" in location_insight : name = location_insight["name"]
+        else: name = str(location_insight["geojson"]["geometry"]["coordinates"][1]) +", "+ str(location_insight["geojson"]["geometry"]["coordinates"][0])
+        if "formatted_address" in location_insight : address = location_insight["formatted_address"]
+        else: address = name
         results.append({"id":location_insight["location_id"],
-                        "name": location_insight["name"],
-                        "address": location_insight["formatted_address"],
+                        "name": name,
+                        "address" : address,
                         "lat": location_insight["geojson"]["geometry"]["coordinates"][1],
                         "lon": location_insight["geojson"]["geometry"]["coordinates"][0],
                         "radius_unit": location_insight["geojson"]["properties"]["radius_unit"],
@@ -112,7 +118,6 @@ def fetch_location_insight_data():
                         "tz": "America/New_York",})
     
     return results
-
 
 def show_map_sidebar_code_examples():
     st.sidebar.markdown("## Code examples")
